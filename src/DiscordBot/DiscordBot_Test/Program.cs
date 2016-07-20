85ea9cdd8c1fa1760469a4f976ef5feeec404868
Andrew Godfrey <Andrew Godfrey>
2016-07-19 22:53:36 -0400

    Fixed parsing issue for commands.

--- src/DiscordBot/DiscordBot_Test/Program.cs
+++ src/DiscordBot/DiscordBot_Test/Program.cs
@@ -63,7 +63,7 @@ namespace DiscordBot_Test
             client.GetService<CommandService>().CreateCommand("greet") //create command greet
                 .Alias(new string[] { "greet", "welcome" }) //add 2 aliases, so it can be run with greet or welcome
                 .Description("Greets a person.") //add description, it will be shown when ~help is used
-                .Parameter("GreetedPerson", ParameterType.Required) //as an argument, we have a person we want to greet
+                .Parameter("GreetedPerson", ParameterType.Unparsed) //as an argument, we have a person we want to greet
                 .Do(async e =>
              {
                  await e.Channel.SendMessage($"{e.User.Name} welcomes {e.GetArg("GreetedPerson")}");
@@ -92,7 +92,7 @@ namespace DiscordBot_Test
             client.GetService<CommandService>().CreateCommand("price") //create command price
                 .Alias(new string[] { "price", "value" }) //add 2 aliases, so it can be run with price or value
                 .Description("Outputs price of item. Please use an underscore to indicate a space in an item name.") //adds description, it will be shown when ~help is used
-                .Parameter("item", ParameterType.Required) //takes in the item we wish to price check
+                .Parameter("item", ParameterType.Unparsed) //takes in the item we wish to price check
                 .Do(async e =>
                 {
                     Console.WriteLine($"Finding Item Price for {e.GetArg("item")}");
@@ -124,6 +124,32 @@ namespace DiscordBot_Test
                     await e.Channel.SendMessage($"|Path 1: {path1}|  |Path 2: {path2}|  |Path 3: {path3}|");//sends a message to channel with the given text
                     Console.WriteLine("Finished finding paths");
                 });
+
+            //HELLO
+            client.GetService<CommandService>().CreateCommand("hello") //create command hello
+                .Alias(new string[] { "hello", "noobs" }) //add 2 aliases
+                .Description("Hello....") //adds description, it will be shown when ~help is used
+                .Parameter("None", ParameterType.Optional) //takes in the item we wish to price check
+                .Do(async e =>
+                {
+                    Console.WriteLine($"Saying Hello");
+                    await e.Channel.SendFile("hello.mp3");//sends a file
+                    Console.WriteLine("Finished finding paths");
+                });
+            
+            //TTS
+            client.GetService<CommandService>().CreateCommand("TextToSpeech") //create command hello
+                .Alias(new string[] { "tts", "say" }) //add 2 aliases
+                .Description("Reads out a message") //adds description, it will be shown when ~help is used
+                .Parameter("Message", ParameterType.Unparsed) //takes in the item we wish to price check
+                .Do(async e =>
+                {
+                    Console.WriteLine($"Performing text to speech with line: {e.GetArg("Message")}");
+                    await e.Message.Channel.SendTTSMessage($"{e.GetArg("Message")}");//Text to speech line
+                    Console.WriteLine("Finished performing text to speech.");
+                });
+
+
 
             //logs all messages in the console
             client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
